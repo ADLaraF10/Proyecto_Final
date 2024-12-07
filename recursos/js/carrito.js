caa51d7b6288f0d081ad64567ccf6969b0b151ea
ADLaraF10 <andres.lara.ttc.038@unilibre.edu.co>
2024-12-06 20:17:39 -0500

    implementacion carrito de compra funcion

---
+++ recursos/js/carrito.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const divisa = "$"
+    const DOMcarrito = document.querySelector('#carrito');
+    const carrito = JSON.parse(localStorage.getItem('carrito'));
+    const baseDeDatos = JSON.parse(localStorage.getItem('baseDeDatos'))
+    console.log(carrito)
+
+    function render(){
+        DOMcarrito.textContent = '';
+        const carritoSinDuplicados = [...new Set(carrito)];
+        carritoSinDuplicados.forEach((item) => {
+            console.log(item)
+            const miItem = baseDeDatos.filter((itemBaseDatos) => {
+                return itemBaseDatos.id === parseInt(item);
+            });
+            const numeroUnidadesItem = carrito.reduce((total, itemId) => {
+                return itemId === item ? total += 1 : total;
+            }, 0);
+            const miNodo = document.createElement('li');
+            miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+            miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0].nombre} - ${miItem[0].precio}${divisa}`;
+            const miBoton = document.createElement('button');
+            miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+            miBoton.textContent = 'X';
+            miBoton.style.marginLeft = '1rem';
+            miBoton.dataset.item = item;
+            miBoton.addEventListener('click', borrarItemCarrito);
+            miNodo.appendChild(miBoton);
+            DOMcarrito.appendChild(miNodo);
+        })
+    }
+
+    function borrarItemCarrito(evento) {
+        const id = evento.target.dataset.item;
+        carrito = carrito.filter((carritoId) => {
+            return carritoId !== id;
+        });
+        renderizarCarrito();
+        guardarCarritoEnLocalStorage();
+        handleCarritoValue(carrito.length);
+    }
+
+    render()
+})
+
--- recursos/js/index.js
+++ recursos/js/index.js
@@ -1,63 +1,86 @@
 
-document.addEventListener('DOMContentLoaded',()=>{
-//variables
-console.log('entre')
-const baseDeDatos = [
-    {
-        id: 1,
-        nombre: 'teclado',
-        descripcion: 'descripcion',
-        precio: 20000,
-        imagen: 'recursos/img/LogoIcono.png',
-        categoria: 'pc'
-    },
-    {
-        id: 2,
-        nombre: 'teclado',
-        descripcion: 'descripcion',
-        precio: 20000,
-        imagen: 'recursos/img/LogoIcono.png',
-        categoria: 'pc'
-    },
-];
-const DOMitems = document.querySelector('#items');
-const divisa = "$"
+document.addEventListener('DOMContentLoaded', () => {
+    /*VARIABLES*/
 
-function render(){
-    baseDeDatos.forEach((dato) => {
-        const miNodo = document.createElement('div');
-        miNodo.classList.add('card', 'col-sm-4','bg-black', 'm-2', 'p-1', 'justify-content-center');
-        const miNodoCardBody = document.createElement('div');
-        miNodoCardBody.classList.add('card-body','bg-dark', 'p-0');
-        const miNodoTitle = document.createElement('h6');
-        miNodoTitle.classList.add('card-title', 'text-white', 'm-2');
-        miNodoTitle.textContent = dato.nombre;
-        const miNodoImagen = document.createElement('img');
-        miNodoImagen.classList.add('img-fluid', 'w-3');
-        miNodoImagen.setAttribute('src', dato.imagen);
-        const miNodoDescripcion = document.createElement('p');
-        miNodoDescripcion.classList.add('card-text', 'text-secondary', 'mx-2');
-        miNodoDescripcion.textContent = dato.descripcion;
-        const miNodoFooter = document.createElement('div')
-        miNodoFooter.classList.add('d-flex','justify-content-between', 'align-items-center')
-        const miNodoPrecio = document.createElement('p');
-        miNodoPrecio.classList.add('card-text', 'text-secondary', 'mx-2');
-        miNodoPrecio.textContent = `${dato.precio}${divisa}`;
-        const miNodoBoton = document.createElement('button');
-        miNodoBoton.classList.add('btn', 'btn-primary', 'm-2');
-        miNodoBoton.textContent = 'Agregar';
-        miNodoBoton.setAttribute('marcador', dato.id);
-        miNodoCardBody.appendChild(miNodoImagen)
-        miNodoCardBody.appendChild(miNodoTitle)
-        miNodoCardBody.appendChild(miNodoDescripcion)
-        miNodoFooter.appendChild(miNodoPrecio)
-        miNodoFooter.appendChild(miNodoBoton)
-        miNodoCardBody.appendChild(miNodoFooter)
-        miNodo.appendChild(miNodoCardBody);
-        DOMitems.appendChild(miNodo)
-    })
-}
+    const baseDeDatos = [
+        {
+            id: 1,
+            nombre: 'teclado',
+            descripcion: 'descripcion',
+            precio: 20000,
+            imagen: 'recursos/img/LogoIcono.png',
+            categoria: 'pc'
+        },
+        {
+            id: 3,
+            nombre: 'teclado',
+            descripcion: 'descripcion',
+            precio: 20000,
+            imagen: 'recursos/img/LogoIcono.png',
+            categoria: 'pc'
+        },
+    ];
+    const DOMitems = document.querySelector('#items');
+    const divisa = "$"
+    let carrito = []
 
-render()
+
+    function render() {
+        baseDeDatos.forEach((dato) => {
+            const miNodo = document.createElement('div');
+            miNodo.classList.add('card', 'col-sm-4', 'bg-black', 'm-2', 'p-1', 'justify-content-center');
+            const miNodoCardBody = document.createElement('div');
+            miNodoCardBody.classList.add('card-body', 'bg-dark', 'p-0');
+            const miNodoTitle = document.createElement('h6');
+            miNodoTitle.classList.add('card-title', 'text-white', 'm-2');
+            miNodoTitle.textContent = dato.nombre;
+            const miNodoImagen = document.createElement('img');
+            miNodoImagen.classList.add('img-fluid', 'w-3');
+            miNodoImagen.setAttribute('src', dato.imagen);
+            const miNodoDescripcion = document.createElement('p');
+            miNodoDescripcion.classList.add('card-text', 'text-secondary', 'mx-2');
+            miNodoDescripcion.textContent = dato.descripcion;
+            const miNodoFooter = document.createElement('div')
+            miNodoFooter.classList.add('d-flex', 'justify-content-between', 'align-items-center')
+            const miNodoPrecio = document.createElement('p');
+            miNodoPrecio.classList.add('card-text', 'text-secondary', 'mx-2');
+            miNodoPrecio.textContent = `${dato.precio}${divisa}`;
+            const miNodoBoton = document.createElement('button');
+            miNodoBoton.classList.add('btn', 'btn-primary', 'm-2');
+            miNodoBoton.textContent = 'Agregar';
+            miNodoBoton.setAttribute('marcador', dato.id);
+            miNodoBoton.addEventListener('click', agregarAlCarrito);
+            miNodoCardBody.appendChild(miNodoImagen)
+            miNodoCardBody.appendChild(miNodoTitle)
+            miNodoCardBody.appendChild(miNodoDescripcion)
+            miNodoFooter.appendChild(miNodoPrecio)
+            miNodoFooter.appendChild(miNodoBoton)
+            miNodoCardBody.appendChild(miNodoFooter)
+            miNodo.appendChild(miNodoCardBody);
+            DOMitems.appendChild(miNodo)
+        })
+    }
+
+    function agregarAlCarrito(evento) {
+        carrito.push(evento.target.getAttribute('marcador'));
+        agregarAlStorage();
+        handleCarritoValue(carrito.length)
+    }
+
+    function agregarAlStorage(){
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+    }
+
+    function AlmacenarDatos(){
+        localStorage.setItem('baseDeDatos', JSON.stringify(baseDeDatos));
+    }
+
+    function handleCarritoValue(value) {
+        const carritoContainer = document.getElementById("carrito-value");
+        carritoContainer.textContent = `${value}`;
+    }
+
+    AlmacenarDatos()
+    render()
 
 });
